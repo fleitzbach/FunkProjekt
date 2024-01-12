118f0f92b7f3a995712e54b89fe802bb04ab0acb
david <david.dengle02@gmail.com>
2024-01-12 11:24:48 +0100

    add all

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- index.svelte -->
+<script>
+  import WorldMap from '$lib/WorldMap.svelte';
+</script>
+
+<main>
+  <WorldMap />‚
+</main>
+
+<style>
+  main {
+    height: 100vh;
+  }
+</style>
