5f6165b9c5efe79568493cc97847a2f798deb37c
hhdhwuus <david.dengle02@gmail.com>
2024-02-16 22:14:26 +0100

    Delete unused files and update API URL

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,10 @@
-export const API_URL = "http://localhost:8000"
+
+import { browser, building, dev, version } from '$app/environment';
+
+let API_URL = 'https://api.wetter.fhy.re';
+
+if (dev) {
+    API_URL = 'http://localhost:8000';
+}
+
+export { API_URL };
